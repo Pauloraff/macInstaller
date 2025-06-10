9ccb1c14afb6b4fe7f236ebedb0688f6b18a31b5
Paulo Raffaelli <pauloraff@gmail.com>
2025-06-10 16:16:46 -0400

    Fixed typo: 'connectionInterruptionHandler' was misspelled.

---
+++ macInstaller/installedapp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  installedapp
+//
+//  Created by Paulo Raffaelli on 6/6/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+}
+
